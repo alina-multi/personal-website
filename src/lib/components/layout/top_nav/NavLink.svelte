4505cd35f05cf842e1e39b0efd8cca0ab31e8d99
Alina <79420032+alina-multi@users.noreply.github.com>
2025-09-01 17:48:39 +0100

    fix navlink

--- src/lib/components/layout/top_nav/NavLink.svelte
+++ src/lib/components/layout/top_nav/NavLink.svelte
@@ -1,21 +1,22 @@
 <script>
-	let { id, label, index, active } = $props();
+  let { id, label, index, active } = $props();
 
-	function scrollToSection(id) {
-		const section = document.getElementById(id);
+  function scrollToSection(id) {
+    const section = document.getElementById(id);
 
-		section?.scrollIntoView({ behavior: 'smooth' });
-		active = id;
-	}
+    section?.scrollIntoView({ behavior: "smooth" });
+    active = id;
+  }
 </script>
 
-<button
-	onclick={() => scrollToSection(id)}
-	class="flex flex-col items-center max-lg:w-full max-lg:py-3 max-lg:text-xl lg:flex-row {active ===
-	id
-		? 'text-white after:scale-x-100 after:bg-teal-400'
-		: ' hover:scale-105 hover:text-white'} transition-transform"
+<a
+  href={`/#${id}`}
+  onclick={() => scrollToSection(id)}
+  class="flex cursor-pointer flex-col items-center max-lg:w-full max-lg:py-3 max-lg:text-xl lg:flex-row {active ===
+  id
+    ? 'text-white after:scale-x-100 after:bg-teal-400'
+    : ' hover:scale-105 hover:text-white'} transition-transform"
 >
-	<span class="text-teal-300">{`0${index + 1}.`}</span>
-	<span class="ml-2 max-lg:text-stone-200">{label}</span>
-</button>
+  <span class="text-teal-300">{`0${index + 1}.`}</span>
+  <span class="ml-2 max-lg:text-stone-200">{label}</span>
+</a>
