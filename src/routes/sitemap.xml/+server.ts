6c7fde76aca53ec2a550ee8bf855e3febd1e59a1
Alina <79420032+alina-multi@users.noreply.github.com>
2025-09-02 17:47:56 +0100

    prerender sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -16,6 +16,9 @@ const staticPages = [
   },
 ];
 
+// Add this to make it prerender
+export const prerender = true;
+
 export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
